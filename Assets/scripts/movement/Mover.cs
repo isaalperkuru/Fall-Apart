fd0c2aa0096e928e025f81001ed20ee606bc007d
Alper <alperkuru98@hotmail.com>
2020-11-01 18:51:40 +0300

    Commit 1
    Basic character movement and Camera align to character

---
+++ Assets/scripts/movement/Mover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    void Update()
+    {
+        if(Input.GetMouseButton(0)) //left click
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+       
+    }
+    private void UpdateAnimator()
+    {
+        //we taking velocity of navmesh
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        //we make velocity increase by the time while moving
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        //z velocity
+        float speed = localVelocity.z;
+        //min velo to max velo informed
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+    }
+    private void MoveToCursor()
+    {
+        //left click laser from cam
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //left click laser info
+        RaycastHit hit;
+        //if we hit something. 
+        //out allows us to return information about the location that a raycast has hit
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            //go to position ray (laser) hit
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+        
+        
+}
