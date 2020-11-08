498595831d05c5d126b9b384db9d7e2a310e3695
Alper <alperkuru98@hotmail.com>
2020-11-08 18:14:40 +0300

    Commit 2
    Assets added. Namespacing. Fighter, PlayerController and CombatTarget scripts added (combat)

---
+++ Assets/scripts/Control/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithMovement()
+        {
+            if (Input.GetMouseButton(0)) //left click
+            {
+                MoveToCursor();
+            }
+        }
+        private void InteractWithCombat()
+        {
+            //We take every ray hits and finding object we can attack
+            //so the other objects cant block the ray hits to enemy
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            //left click laser info
+            RaycastHit hit;
+            //if we hit something. 
+            //out allows us to return information about the location that a raycast has hit
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+                //go to position ray (laser) hit
+
+            }
+
+           
+        }
+        static Ray GetMouseRay()
+        {
+            //left click laser from cam
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
+
